04163bf3cff9501efeea00833f769c5548c0b8db
HUIOS <services.huios@gmail.com>
2019-10-31 12:04:16 +0100

    les servlets

--- multimodule-web/src/main/java/servlets/PersonneServlet.java
+++ multimodule-web/src/main/java/servlets/PersonneServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class PersonneServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+        
     /**
      * @see HttpServlet#HttpServlet()
      */
